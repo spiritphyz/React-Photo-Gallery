ea1ecbd3a820240a82ea14b2ad94a212da8cca4d
spiritphyz <tonyle@ocf.berkeley.edu>
2017-04-17 16:35:54 -0700

    Add 3 images with text

--- numbered-gallery/src/components/Store.js
+++ numbered-gallery/src/components/Store.js
@@ -1,26 +1,26 @@
 export const Store = {
-  imageData: [
+  imgData: [
     {
       id: '1000',
-      url: '',
-      title: 'Blood Analysis',
-      description: '',
-      width: 0,
-      height: 0,
+      url: 'http://spiritphyz.io/img/gg/centrifuge-insert_1920x1280.jpg',
+      title: 'Centrifuge Applications for Blood Separation',
+      description: 'The blood is collected in tubes or bags, depending on the prescribed test from the doctor. Tubes can vary in size from 1.2mL to 15mL, while bags are usually around 300mL to 500mL. The role of a centrifuge in testing blood is to separate whole blood into its various components.',
+      width: 1920,
+      height: 1280,
     },
     {
       id: '2000',
-      url: '',
-      title: 'Receiving Dialysis',
-      description: '',
-      width: 0,
-      height: 0,
+      url: 'http://spiritphyz.io/img/gg/pipette-prepare_1920x1280.jpg',
+      title: 'Pipette Usage Technique',
+      description: 'The easiest way to test your technique is by using a pipette with an analytical balance. Pipette deionized water into a tared container on the balance to check your accuracy and reproducibility. One-thousand microliters of water weighs about 1,000 milligrams, which can be easily measured on an analytical balance.',
+      width: 1920,
+      height: 1280,
     },
     {
       id: '3000',
-      url: '',
-      title: 'Receiving Dialysis',
-      description: '',
+      url: 'http://spiritphyz.io/img/gg/chair-black-empty_19280x1280.jpg',
+      title: 'Diagnostic Medical Imaging',
+      description: 'Nuclear medicine technologiests prepare and administer radioactive drugs that make abnormal areas of the body appear different than normal areas, use special cameras to detect gamma rays emitted by the radioactive drugs in a patient’s body, explain imaging procedures to patients and answer questions.',
       width: 0,
       height: 0,
     }
@@ -33,9 +33,9 @@ export const Store = {
     this.position = newPosition;
   },
   getImages: function() {
-    return this.imageData;
+    return this.imgData;
   },
-  setImages: function(newImageData) {
-    this.imageData = newImageData;
+  setImages: function(newimgData) {
+    this.imgData = newimgData;
   }
 };
