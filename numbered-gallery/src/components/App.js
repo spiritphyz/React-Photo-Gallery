fb5321df8952e0d4b34eb27e28a50fcd5360e6b8
spiritphyz <tonyle@ocf.berkeley.edu>
2017-04-19 15:21:01 -0700

    Refactor to use MediaObject component

--- numbered-gallery/src/components/App.js
+++ numbered-gallery/src/components/App.js
@@ -1,36 +1,24 @@
 import React from 'react';
 import { Store } from './Store';
-import Container from './Container';
 import MediaObject from './MediaObject';
-import MediaHeading from './MediaHeading';
-import MediaText from './MediaText';
 import '../css/app.css';
 
-let title = Store.imgData[1].title;
 let imgUrl = Store.imgData[1].url;
+let alt = Store.imgData[1].alt;
+let heading = Store.imgData[1].title;
 let text = Store.imgData[1].description;
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
+      <div className="app">
         <MediaObject
-          className="col mr-2 ml-2" 
-          text={ text }>
-            <MediaHeading className="mt-5" heading={ title }></MediaHeading>
-            <MediaText text={ text }></MediaText>
-        </MediaObject> 
-        <Container
-          title={ title } 
-          className="greenbg"
-          onClick={ e => console.log('app was clicked') }
+          imgUrl={ imgUrl }
+          alt={ alt }
+          heading={ heading }
+          text={ text }
         >
-          <img
-            src={ imgUrl }
-            style={ {width: "100%"} }
-            alt={ title }
-          />
-        </Container>
+        </MediaObject> 
       </div>
     );
   }
