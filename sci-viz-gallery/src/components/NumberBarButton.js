b72da005b41108e35214d4b7a272eea20a967741
spiritphyz <tonyle@ocf.berkeley.edu>
2017-05-11 14:26:36 -0700

    Add titles to hamburger menu items
    Titles are only shown when navbar is collapsed (like on skiny mobile phone screens). For wide screens, the titles are not shown because the original design spec showed only numbers on the horizontal numbered navigation bar.

--- sci-viz-gallery/src/components/NumberBarButton.js
+++ sci-viz-gallery/src/components/NumberBarButton.js
@@ -4,28 +4,37 @@ import classNames from 'classnames';
 import '../css/numberbar-button.css';
 
 export default function NumberBarButton(props) {
-  const { label, index, className, anchorClassName, buttonClick, currPos, children, ...moreProps } = props;
+  const { label, responsiveLabel, index, className, anchorClassName, asideClassName, buttonClick, currPos, children, ...moreProps } = props;
   const imgIdx = index - 1;
   let joinClasses;
   let joinAnchorClasses;
+  let joinAsideClasses;
   let screenReaderTag = '';
 
   if (currPos === imgIdx) {
     joinClasses = classNames('numberbar-button', 'underline', className);
     joinAnchorClasses = classNames('active', anchorClassName);
+    joinAsideClasses = classNames(asideClassName);
     screenReaderTag = (
       <span className="sr-only">(current)</span>
     );
   } else {
     joinClasses = classNames('numberbar-button', className);
     joinAnchorClasses = classNames(anchorClassName);
+    joinAsideClasses = classNames(asideClassName);
   }
 
 
   return (
     <div className={ joinClasses } onClick={ () => buttonClick(imgIdx) } { ...moreProps }>
       <li>
-        <a href="#" className={ joinAnchorClasses }>{ label }{ screenReaderTag }</a>
+        <a href="#" className={ joinAnchorClasses }>
+          { label }
+          { screenReaderTag }
+          <aside className={ joinAsideClasses }>
+            { responsiveLabel }
+          </aside>
+        </a>
       </li>
       { children }
     </div>
@@ -34,9 +43,11 @@ export default function NumberBarButton(props) {
 
 NumberBarButton.propTypes = {
   label: PropTypes.string,
+  responsiveLabel: PropTypes.string,
   index: PropTypes.number,
   className: PropTypes.string,
   anchorClassName: PropTypes.string,
+  asideClassName: PropTypes.string,
   buttonClick: PropTypes.func,
   currPos: PropTypes.number,
   children: PropTypes.node
