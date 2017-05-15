21965063f3ee44f7f2ae710b10b9673849d73d69
spiritphyz <tonyle@ocf.berkeley.edu>
2017-05-14 22:46:13 -0700

    Refactor img locations for increased speed
    Use higher amount of concurrent connections by leveraging simultaneous
    browser requests to a different domain for images: www.ocf.berkeley.edu

--- sci-viz-gallery/src/components/Store.js
+++ sci-viz-gallery/src/components/Store.js
@@ -5,7 +5,7 @@ export const Store = {
   imgData: [
     {
       id: '1000',
-      url: 'http://spiritphyz.io/img/bsr/b-sheet-illustration_773x494.jpg',
+      url: 'https://www.ocf.berkeley.edu/~tonyle/sci-viz-gallery/img/b-sheet-illustration_773x494.jpg',
       pdfUrl: '',
       alt: 'beta sheet of protein structures',
       title: 'Protein Secondary Structures: Beta Pleated Sheets',
@@ -17,7 +17,7 @@ export const Store = {
     },
     {
       id: '2000',
-      url: 'http://spiritphyz.io/img/bsr/cell-gates_1000x661.png',
+      url: 'https://www.ocf.berkeley.edu/~tonyle/sci-viz-gallery/img/cell-gates_1000x661.png',
       pdfUrl: 'https://www.ocf.berkeley.edu/~tonyle/sci-viz-gallery/Tony-TC-Le-Ion-Channels-Illustration.pdf',
       alt: 'ion channel proteins being activated',
       title: 'Ion Channel Gating Mechanisms',
@@ -29,7 +29,7 @@ export const Store = {
     },
     {
       id: '3000',
-      url: 'http://spiritphyz.io/img/bsr/photosynthesis_1000x583.png',
+      url: 'https://www.ocf.berkeley.edu/~tonyle/sci-viz-gallery/img/photosynthesis_1000x583.png',
       pdfUrl: 'https://www.ocf.berkeley.edu/~tonyle/sci-viz-gallery/Tony-TC-Le-Photosynthesis-Illustrations.pdf',
       alt: 'electrical generation in plants and photovoltaics',
       title: 'Lean, Mean, Green Machines',
@@ -41,7 +41,7 @@ export const Store = {
     },
     {
       id: '4000',
-      url: 'http://spiritphyz.io/img/bsr/CRISPR-illustration_1000x961.jpg',
+      url: 'https://www.ocf.berkeley.edu/~tonyle/sci-viz-gallery/img/CRISPR-illustration_1000x961.jpg',
       pdfUrl: 'https://www.ocf.berkeley.edu/~tonyle/sci-viz-gallery/Tony-TC-Le-CRISPR-Illustration.pdf',
       alt: 'high-level overview of CRISPR mechanism',
       title: 'The Evolution of Bacterial Immunity',
@@ -54,7 +54,7 @@ export const Store = {
     {
       id: '5000',
       url: 'http://spiritphyz.io/img/bsr/nobel-tradition-page-layout_1028x668.png',
-      pdfUrl: 'https://www.ocf.berkeley.edu/~tonyle/sci-viz-gallery/Tony-TC-Le-Print-Design.pdf',
+      pdfUrl: 'https://www.ocf.berkeley.edu/~tonyle/sci-viz-gallery/img/nobel-tradition-page-layout_1028x668.png',
       alt: 'timeline of Nobel laureates',
       title: `A History of UC Berkeley\u02bcs Laureates`,
       subheading: 'Client: University of California, Berkeley',
